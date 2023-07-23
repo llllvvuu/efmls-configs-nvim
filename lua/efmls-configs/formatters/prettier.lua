4ce85e1a9ec13bba22fc96d478cf03c1ecf4f532
L Lllvvuu <git@llllvvuu.dev>
2023-07-22 17:37:51 -0700

    feat(prettier, prettier_d): textDocument/rangeFormatting

--- lua/efmls-configs/formatters/prettier.lua
+++ lua/efmls-configs/formatters/prettier.lua
@@ -1,9 +1,25 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'prettier'
-local command = string.format('%s --stdin --stdin-filepath ${INPUT}', fs.executable(formatter, fs.Scope.NODE))
+local command = string.format(
+  '%s --stdin --stdin-filepath ${INPUT} ${--range-start:charStart} '
+    .. '${--range-end:charEnd} ${--tab-width:tabSize} ${--use-tabs:!insertSpaces}',
+  fs.executable(formatter, fs.Scope.NODE)
+)
+local root_markers = {
+  ".prettierrc",
+  ".prettierrc.json",
+  ".prettierrc.js",
+  ".prettierrc.yml",
+  ".prettierrc.yaml",
+  ".prettierrc.json5",
+  ".prettierrc.mjs",
+  ".prettierrc.cjs",
+  ".prettierrc.toml",
+}
 
 return {
   formatCommand = command,
   formatStdin = true,
+  rootMarkers = root_markers,
 }
